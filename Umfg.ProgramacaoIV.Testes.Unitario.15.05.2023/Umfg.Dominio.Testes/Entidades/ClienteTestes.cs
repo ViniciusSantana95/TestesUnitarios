e47589726be531ec801c0c8a28f41021e31d2d3f
vinicius <kvinin8@gmail.com>
2023-06-04 22:36:36 -0300

    testes unitarios

---
+++ Umfg.ProgramacaoIV.Testes.Unitario.15.05.2023/Umfg.Dominio.Testes/Entidades/ClienteTestes.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Umfg.Dominio.Entidades;
+
+namespace Umfg.Dominio.Testes.Entidades
+{
+    [TestClass]
+    public sealed class ClienteTestes
+    {
+        private const string _firstOwner = "Vinicius Gabriel";
+        private const string _secondOwner = "Elizete Rocha";
+        private const string _category = "Cliente";
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_Construtor_Sucesso()
+        {
+            try
+            {
+                var cliente = new Cliente("59958894204", "TESTE");
+                Assert.AreEqual("TESTE", cliente.Nome);
+                Assert.AreEqual("59958894204", cliente.Documento);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_DocumentoNull_ThrowArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Cliente(null, "TESTE"));
+        }
+
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_NomeNull_ThrowArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Cliente("10494265922", null));
+        }
+
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_DocumentoVazio_ThrowArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Cliente("", "TESTE"));
+        }
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_NomeVazio_ThrowArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Cliente("10494265922", ""));
+        }
+    }
+}
---
+++ Umfg.ProgramacaoIV.Testes.Unitario.15.05.2023/Umfg.Dominio/Entidades/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umfg.Dominio.Entidades
+{
+    public sealed class Cliente
+    {
+        public string Documento { get; private set; }
+        public string Nome { get; private set; }
+
+        public Cliente(string documento, string nome)
+        {
+            if (string.IsNullOrEmpty(documento))
+                throw new ArgumentException("O valor do documento não pode estar vazio.", nameof(documento));
+
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("O valor do nome não pode estar vazio.", nameof(nome));
+
+            Documento = documento ?? throw new ArgumentNullException(nameof(documento));
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome)); ;
+        }
+    }
+}
